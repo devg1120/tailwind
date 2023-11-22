985ad0420bdb2742cc1dc9616df17cf700a901f7
vboxuser <vboxuser@ubuntu-server-01.myguest.virtualbox.org>
2023-11-22 17:34:19 +0900

    first

---
+++ tailwind_css/start_lighttpd.sh
@@ -0,0 +1,5 @@
+
+cd public
+
+lighttpd -D -f ../_lighttpd.conf
+
---
+++ tailwind_css/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+
+/*
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+*/
+
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./public/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
